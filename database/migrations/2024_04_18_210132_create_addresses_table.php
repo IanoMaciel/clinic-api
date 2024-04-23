b3715715954e43651d0af9676440d00f64583bb1
IanoMaciel <ianomaciel6385@gmail.com>
2024-04-23 08:44:02 -0400

    feat: criação da migração de endereço

---
+++ database/migrations/2024_04_18_210132_create_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('cep');
+            $table->string('uf');
+            $table->string('city');
+            $table->string('street');
+            $table->string('number');
+            $table->string('neighborhood');
+            $table->string('complement');
+            $table->string('reference');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
