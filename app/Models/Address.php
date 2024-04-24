5c1f263037dcc5a06c7786b4c38af4e15629df4f
IanoMaciel <ianomaciel6385@gmail.com>
2024-04-24 10:47:12 -0400

    feat: criação do modelo e regras de validação para address

---
+++ app/Models/Address.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'cep',
+        'uf',
+        'city',
+        'street',
+        'number',
+        'neighborhood',
+        'complement',
+        'reference',
+    ];
+
+    public function rules() {
+        return [
+            'customer_id' => 'required:customers,id',
+            'cep' => 'required|string|format_cep',
+            'uf' => 'required|string|uf',
+            'city' => 'required|string',
+            'street' => 'required|string',
+            'number' => 'nullable|string',
+            'neighborhood' => 'required|string',
+            'complement' => 'nullable|string',
+            'reference' => 'nullable|string',
+        ];
+    }
+}
