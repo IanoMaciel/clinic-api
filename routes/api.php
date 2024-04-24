c06be41a8af845f603c2aea4f6a737f5a2bfdc6a
IanoMaciel <ianomaciel6385@gmail.com>
2024-04-24 10:46:19 -0400

    feat: criação da rota para cadastrar endereço de cliente

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('customer', 'CustomerController');
+Route::apiResource('address', 'AddressController');
