1ced98b28cf2f7a6310397e2a1548fcdd3bd6a92
Iano Maciel <ianomaciel6385@gmail.com>
2024-05-16 19:56:52 -0400

    refactor: método store e update atualizado para atender os requisitos de software

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,7 +36,12 @@ class ProductController extends Controller {
      * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request): \Illuminate\Http\JsonResponse {
+        // verificar se o nome existe
+        $nameExists = $this->product->where('name', $request->name)->exists();
+        if($nameExists) return response()->json(['error' => 'Product name must be unique'], 422);
+
         $request->validate($this->product->rules());
+
         $product = $this->product->create($request->all());
         return response()->json($product, 201);
     }
@@ -62,9 +67,22 @@ class ProductController extends Controller {
      */
     public function update(Request $request, $id): \Illuminate\Http\JsonResponse {
         $product = $this->product->find($id);
+
         if (!$product) abort(404, 'Product not found');
 
         $request->validate($this->product->rules());
+
+        // Verifica se o nome fornecido no request já existe para outro produto
+        if ($request->has('name') && $request->name !== $product->name) {
+            $existingProduct = $this->product->where('name', $request->name)->first();
+            if ($existingProduct && $existingProduct->id !== $product->id) {
+                return response()->json(['error' => 'Product name must be unique.'], 422);
+            }
+        }
+
+        // Se o nome não estiver presente no pedido ou for o mesmo que o atual, não o atualize
+        if (!$request->has('name') || $request->name === $product->name) $request->request->remove('name');
+
         $product->update($request->all());
 
         return response()->json($product, 200);
