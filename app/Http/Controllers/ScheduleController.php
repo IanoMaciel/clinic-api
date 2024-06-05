3957465b769b4b85f36a5f2930abc3f57ee31f44
Iano Maciel <ianomaciel6385@gmail.com>
2024-06-05 11:14:23 -0400

    feat: criação do método store

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Scheduling;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
@@ -20,7 +21,7 @@ class ScheduleController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        //
+
     }
 
     /**
@@ -31,13 +32,29 @@ class ScheduleController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request) {
-        // 1. Adicionar a auth
+        // 1. checks if the user has authorization
+        $authUser = auth()->user();
+        $isAdmin = $authUser->getAttribute('is_admin');
+        $isCommon = $authUser->getAttribute('is_common');
 
+        // if you not user admin and common, it should return 401 (Unauthorized)
+        if (!$isAdmin && !$isCommon) return response()->json(['error' => 'Unauthorized'], 401);
 
-        // 2. Cria um novo agendamento
+        // 3. valida a solicitação está usando as regras definidas no modelo
         $request->validate($this->schedule->rules());
-        $schedule = $this->schedule->create($request->all());
 
+        // 2. checks if date and time is valid
+        $dateTime = Carbon::parse($request->get('date_time'));
+
+        // verifica se a data e hora estão no passado
+        if ($dateTime->isPast()) return response()->json(['error' => 'Connot schedule in the past'], 400);
+
+        //4. verifica se horário já está ocupado
+        $existingSchedule = $this->schedule->query()->where('date_time', $dateTime)->first();
+        if ($existingSchedule) return response()->json(['error' => 'Time slot already token'], 400);
+
+        // 5. create a new schedule
+        $schedule = $this->schedule->create($request->all());
         return response()->json($schedule, 201);
     }
 
