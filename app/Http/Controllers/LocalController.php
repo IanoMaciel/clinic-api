2b358fe6598a89136f4e67076c8eaf7800609422
Iano Maciel <ianomaciel6385@gmail.com>
2024-06-14 08:36:19 -0400

    refactor: inclusão dos demais métodos

--- app/Http/Controllers/LocalController.php
+++ app/Http/Controllers/LocalController.php
@@ -15,7 +15,7 @@ class LocalController extends Controller {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index() {
         $locals = $this->local->all();
@@ -26,7 +26,7 @@ class LocalController extends Controller {
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request) {
         $local = $this->local->create($request->all());
@@ -37,7 +37,7 @@ class LocalController extends Controller {
      * Display the specified resource.
      *
      * @param Integer $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id) {
         $local = $this->local->find($id);
@@ -47,36 +47,31 @@ class LocalController extends Controller {
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Local  $local
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Local $local)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
+     * @param  Integer $id
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Local  $local
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, Local $local)
-    {
-        //
+    public function update(Request $request, $id) {
+        $local = $this->local->query()->find($id);
+        if (!$local) return response()->json(['error' => 'Local not found'], 404);
+
+        $local->update($request->all());
+
+        return response()->json($local, 201);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Local  $local
-     * @return \Illuminate\Http\Response
+     * @param Integer $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(Local $local)
-    {
-        //
+    public function destroy($id) {
+        $local = $this->local->query()->find($id);
+        if (!$local) return response()->json(['error' => 'Local not found'], 404);
+        $local->delete();
+        return response()->json(null, 204);
     }
 }
