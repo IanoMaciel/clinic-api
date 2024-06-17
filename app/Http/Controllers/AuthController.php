0e0490af35c9676c62b73047084831f416210d86
Iano Maciel <ianomaciel6385@gmail.com>
2024-06-17 17:23:33 -0400

    refactor: ajuste no controller auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller {
     /**
@@ -12,16 +14,29 @@ class AuthController extends Controller {
         $credentials = $request->all(['email', 'password']);
         $token = auth('api')->attempt($credentials);
 
-        if ($token) {
-            return response()->json(['token' => $token]);
-        } else {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$token) {
+            return response([
+                'error' => 'Invalid credentials',
+                Response::HTTP_UNAUTHORIZED
+            ]);
         }
+
+        $cookie = cookie('jwt', $token, 60 * 24); // 1 day
+
+        return response([
+            'message' => 'success'
+        ])->withCookie($cookie);
     }
 
     public function logout() {
+        // Invalida o token JWT
         auth('api')->logout();
-        return response()->json(['message' => 'Successfully logged out']);
+
+        // Cria um cookie expirado para remover o cookie existente
+        $cookie = Cookie::forget('jwt');
+
+        // Retorna a resposta de logout com o cookie removido
+        return response()->json(['message' => 'Successfully logged out'])->withCookie($cookie);
     }
 
     public function refresh() {
@@ -31,7 +46,6 @@ class AuthController extends Controller {
     }
 
     public function me() {
-        $user = auth()->user();
-        return response()->json($user);
+        return auth()->user();
     }
 }
