927ec2ae349d8fe276538531110554781ec6f38d
Iano Maciel <ianomaciel6385@gmail.com>
2024-07-27 10:03:18 -0400

    feat: criação do método store

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class CategoryController extends Controller {
+
+    protected $category;
+
+    public function __construct(Category $category) {
+        $this->category = $category;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request  $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse{
+        $request->validate($this->category->rules(), $this->category->feedback());
+        try {
+            $category = $this->category->query()->create($request->all());
+            return response()->json($category, 201);
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => 'Error processing request',
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
