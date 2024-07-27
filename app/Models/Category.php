f29e844d0fdb4de3b542ea80509db0f0c9bc600d
Iano Maciel <ianomaciel6385@gmail.com>
2024-07-27 10:02:27 -0400

    feat: model e migração de categoria

---
+++ app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable = ['category'];
+
+    public function rules () {
+        return [
+            'category' => 'required|string'
+        ];
+    }
+
+    public function feedback () {
+       return [
+           'category.required' => 'Categoria é um campo obrigatório.',
+           'category.string' => 'Categoria deve ser do tipo string.'
+       ];
+    }
+
+    // relationship
+}
